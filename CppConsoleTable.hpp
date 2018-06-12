57e2754996694f8e3d7a29046a67df9209779fc5
Oradle <d.bogdan99@gmail.com>
2018-06-12 03:44:05 +0300

    Added possibility to assing string with multilines, small fixes, refactored
    Added possibility to assing string with multilines (with '\n'), fixed addRow, addColumn, assing functions, added function for reversed (>>) output of table, refactored

--- CppConsoleTable.hpp
+++ CppConsoleTable.hpp
@@ -29,13 +29,13 @@ SOFTWARE.
 #ifndef _CPP_CONSOLE_TABLE_
 #define _CPP_CONSOLE_TABLE_
 
-#include <iostream>
-#include <map>
-#include <string>
-#include <iomanip>
-#include <algorithm>
-#include <vector>
-#include <type_traits>
+#include <algorithm> // max
+#include <iomanip> // setfill, setw, right, left
+#include <iostream> // ostream, endl
+#include <map> // map
+#include <string> // string, to_string, const_iterator
+#include <type_traits> // enable_if, is_arithmetic, is_same, remove_pointer
+#include <vector> // vector
 
 namespace samilton {
 	// force declaration
@@ -48,35 +48,84 @@ namespace samilton {
 			_parent = parent;
 		}
 
+		void clear() {
+			if (_str != nullptr) {
+				if (_lineCount > 1) {
+					delete[] _str;
+				}
+				else {
+					delete _str;
+				}
+
+				_str = nullptr;
+				_lineCount = 1;
+			}
+		}
+
 		ConsoleString &operator=(const std::string &val) {
-			_str = val;
+			clear();
+			_parseString(val);
 			return *this;
 		}
 
 		ConsoleString &operator=(const char *val) {
-			_str = val;
+			clear();
+			_parseString(std::string(val));
 			return *this;
 		}
 
 		template<class T, class = typename std::enable_if<std::is_arithmetic<T>::value>::type>
 		ConsoleString &operator=(const T &val) {
-			_str = std::to_string(val);
+			clear();
+			_str = new std::string;
+
+			*_str = std::to_string(val);
 			return *this;
 		}
 
 		ConsoleString &operator=(const bool &val) {
+			clear();
+			_str = new std::string;
+
 			if (val)
-				_str = "true";
+				*_str = "true";
 			else
-				_str = "false";
+				*_str = "false";
 
 			return *this;
 		}
+
+		~ConsoleString() {
+			clear();
+		}
 	private:
 		friend std::ostream &operator<<(std::ostream &stream, ConsoleTable &table);
+		void _parseString(const std::string &val) {
+			std::vector<std::string::const_iterator> lineEndingMarks;
+			for (auto i = val.begin(); i < val.end(); ++i) {
+				if (*i == '\n') {
+					++_lineCount;
+					lineEndingMarks.push_back(i);
+				}
+			}
+
+			if (_lineCount > 1) {
+				_str = new std::string[_lineCount];
+
+				_str[0].assign(val.begin(), lineEndingMarks[0]);
+				for (size_t i = 1; i < lineEndingMarks.size(); ++i) {
+					_str[i].assign(lineEndingMarks[i - 1] + 1, lineEndingMarks[i]);
+				}
+				_str[_lineCount - 1].assign(lineEndingMarks[_lineCount - 2] + 1, val.end());
+			}
+			else {
+				_str = new std::string(val);
+			}
+		}
 
 		ConsoleRow *_parent;
-		std::string _str;
+		std::string *_str = nullptr;
+		size_t _lineCount = 1;
 	};
 
 	class ConsoleRow {
@@ -148,9 +197,9 @@ namespace samilton {
 		}
 
 		template<class T,
-		class = typename std::enable_if<std::is_arithmetic<T>::value ||
-		std::is_same<std::string, T>::value>::type>
-		void addRow(const T *row, const size_t &size) {
+		class = typename std::enable_if<std::is_arithmetic<typename std::remove_pointer<T>::type>::value ||
+		std::is_same<std::string, typename std::remove_pointer<T>::type>::value>::type>
+		void addRow(const T row, const size_t &size) {
 			const size_t tmp = _rowSize;
 			for (size_t i = 0; i < size; i++) {
 				(*this)[tmp][i] = row[i];
@@ -169,9 +218,9 @@ namespace samilton {
 		}
 
 		template<class T,
-		class = typename std::enable_if<std::is_arithmetic<T>::value ||
-		std::is_same<std::string, T>::value>::type>
-		void addColumn(const T *column, const size_t &size) {
+		class = typename std::enable_if<std::is_arithmetic<typename std::remove_pointer<T>::type>::value ||
+		std::is_same<std::string*, T>::value>::type>
+		void addColumn(const T column, const size_t &size) {
 			const size_t tmp = _columnSize;
 			for (size_t i = 0; i < size; i++) {
 				(*this)[i][tmp] = column[i];
@@ -189,6 +238,16 @@ namespace samilton {
 					(*this)[i][j] = table[i][j];
 		}
 
+		template<class T,
+		class = typename std::enable_if<std::is_arithmetic<T>::value ||
+		std::is_same<std::string, typename std::remove_pointer<typename std::remove_pointer<T>::type>::type>::value>::type>
+		void assign(T table, const size_t &rowSize, const size_t &columnSize) {
+			clear();
+			for (size_t i = 0; i < columnSize; i++)
+				for (size_t j = 0; j < rowSize; j++)
+					(*this)[i][j] = table[i][j];
+		}
+		
 		void clear() {
 			for (auto &element : _tableData) {
 				delete element.second;
@@ -222,16 +281,20 @@ namespace samilton {
 
 		ConsoleRow &operator[](const size_t row);
 
+		friend std::ostream &operator>>(ConsoleTable &table, std::ostream &stream) {
+			return stream << table;
+		}
+
 		friend std::ostream &operator<<(std::ostream &stream, ConsoleTable &table);
 	private:
 		friend ConsoleString &ConsoleRow::operator[](const size_t column);
 
-		void _fillStreamByChar(std::ostream &stream, const char &fillChar, const size_t &lenght) {
+		static void _fillStreamByChar(std::ostream &stream, const char &fillChar, const size_t &lenght) {
 			if (lenght > 0)
 				stream << std::setfill(fillChar) << std::setw(lenght);
 		}
 
-		void _fillStreamByChar(std::ostream &stream, const char &fillChar, const char &endChar, const size_t &lenght) {
+		static void _fillStreamByChar(std::ostream &stream, const char &fillChar, const char &endChar, const size_t &lenght) {
 			if (lenght > 0)
 				stream << std::setfill(fillChar) << std::setw(lenght) << endChar;
 		}
@@ -269,7 +332,7 @@ namespace samilton {
 
 	inline std::ostream &operator<<(std::ostream &stream, ConsoleTable &table) {
 		// Return if table is empty
-		if (table._tableData.size() == 0)
+		if (table._tableData.empty())
 			return stream;
 		
 		// Calculation width of every column
@@ -278,15 +341,34 @@ namespace samilton {
 			size_t tmp = 1;
 			for (size_t j = 0; j < table._rowSize; j++) {
 				if (table._tableData[j] != nullptr && table._tableData[j]->_rowData[i] != nullptr) {
-					tmp = std::max(tmp, table._tableData[j]->_rowData[i]->_str.size());
+					if (table._tableData[j]->_rowData[i]->_lineCount == 1) {
+						tmp = std::max(tmp, table._tableData[j]->_rowData[i]->_str->size());
+					}
+					else {
+						for (size_t k = 0; k < table._tableData[j]->_rowData[i]->_lineCount; ++k) {
+							tmp = std::max(tmp, table._tableData[j]->_rowData[i]->_str[k].size());
+						}
+					}
 				}
 			}
 			columnWidth.push_back(tmp);
 		}
 
+		// Calculation height of every row
+		std::vector<size_t> rowHeight;
+		for (size_t i = 0; i < table._rowSize; i++) {
+			size_t tmp = 1;
+			for (size_t j = 0; j < table._columnSize; j++) {
+				if (table._tableData[i] != nullptr && table._tableData[i]->_rowData[j] != nullptr) {
+					tmp = std::max(tmp, table._tableData[i]->_rowData[j]->_lineCount);
+				}
+			}
+			rowHeight.push_back(tmp);
+		}
+
 		// Top border
 		stream << std::right << table._chars.topLeft;
-		table._fillStreamByChar(stream, table._chars.topDownSimple, columnWidth[0] + 1 + table._leftIndent + table._rightIndent);
+		ConsoleTable::_fillStreamByChar(stream, table._chars.topDownSimple, columnWidth[0] + 1 + table._leftIndent + table._rightIndent);
 
 		if (table._columnSize != 1) {
 			for (size_t i = 1; i < table._columnSize; i++) {
@@ -297,57 +379,69 @@ namespace samilton {
 
 		// Elements and middle borders
 		for (size_t i = 0; i < table._rowSize; i++) {
-			for (size_t j = 0; j < table._columnSize; j++) {
-				if (table._tableData[i] != nullptr && table._tableData[i]->_rowData[j] != nullptr) {
-					if (table._alignment == ConsoleTable::Alignment::centre) {
-						const size_t tmp = columnWidth[j] - table._tableData[i]->_rowData[j]->_str.size();
-						size_t leftAlignmentIndent, rightAlignmentIndent;
-						if (tmp % 2) {
-							leftAlignmentIndent = tmp / 2;
-							rightAlignmentIndent = tmp / 2 + 1;
+			for (size_t k = 0; k < rowHeight[i]; ++k) {
+				for (size_t j = 0; j < table._columnSize; j++) {
+					if (table._tableData[i] != nullptr && table._tableData[i]->_rowData[j] != nullptr && k < table._tableData[i]->_rowData[j]->_lineCount) {
+						if (table._alignment == ConsoleTable::Alignment::centre) {
+							const size_t tmp = k < table._tableData[i]->_rowData[j]->_lineCount
+								                   ? columnWidth[j] - table._tableData[i]->_rowData[j]->_str[k].size()
+								                   : columnWidth[j] - table._tableData[i]->_rowData[j]->_str->size();
+
+							size_t leftAlignmentIndent, rightAlignmentIndent;
+							if (tmp % 2) {
+								leftAlignmentIndent = tmp / 2;
+								rightAlignmentIndent = tmp / 2 + 1;
+							}
+							else {
+								leftAlignmentIndent = rightAlignmentIndent = tmp / 2;
+							}
+
+							stream << table._chars.leftRightSimple;
+							ConsoleTable::_fillStreamByChar(stream, ' ', ' ', table._leftIndent + leftAlignmentIndent);
+
+							if (k < table._tableData[i]->_rowData[j]->_lineCount)
+								stream << table._tableData[i]->_rowData[j]->_str[k];
+							else
+								stream << *table._tableData[i]->_rowData[j]->_str;
+
+							ConsoleTable::_fillStreamByChar(stream, ' ', ' ', table._rightIndent + rightAlignmentIndent);
 						}
 						else {
-							leftAlignmentIndent = rightAlignmentIndent = tmp / 2;
+							if (table._alignment == ConsoleTable::Alignment::left)
+								stream << std::left;
+							else if (table._alignment == ConsoleTable::Alignment::right)
+								stream << std::right;
+
+							stream << table._chars.leftRightSimple;
+							ConsoleTable::_fillStreamByChar(stream, ' ', ' ', table._leftIndent);
+
+							ConsoleTable::_fillStreamByChar(stream, ' ', columnWidth[j]);
+							
+							if (k < table._tableData[i]->_rowData[j]->_lineCount)
+								stream << table._tableData[i]->_rowData[j]->_str[k];
+							else
+								stream << *table._tableData[i]->_rowData[j]->_str;
+
+							ConsoleTable::_fillStreamByChar(stream, ' ', ' ', table._rightIndent);
 						}
-
-						stream << table._chars.leftRightSimple;
-						table._fillStreamByChar(stream, ' ', ' ', table._leftIndent + leftAlignmentIndent);
-
-						stream << table._tableData[i]->_rowData[j]->_str;
-
-						table._fillStreamByChar(stream, ' ', ' ', table._rightIndent + rightAlignmentIndent);
 					}
 					else {
-						if (table._alignment == ConsoleTable::Alignment::left)
-							stream << std::left;
-						else if (table._alignment == ConsoleTable::Alignment::right)
-							stream << std::right;
-
 						stream << table._chars.leftRightSimple;
-						table._fillStreamByChar(stream, ' ', ' ', table._leftIndent);
+						ConsoleTable::_fillStreamByChar(stream, ' ', ' ', table._leftIndent);
 
-						table._fillStreamByChar(stream, ' ', columnWidth[j]);
-						stream << table._tableData[i]->_rowData[j]->_str;
+						ConsoleTable::_fillStreamByChar(stream, ' ', columnWidth[j]);
+						stream << ' ';
 
-						table._fillStreamByChar(stream, ' ', ' ', table._rightIndent);
+						ConsoleTable::_fillStreamByChar(stream, ' ', ' ', table._rightIndent);
 					}
 				}
-				else {
-					stream << table._chars.leftRightSimple;
-					table._fillStreamByChar(stream, ' ', ' ', table._leftIndent);
-
-					table._fillStreamByChar(stream, ' ', columnWidth[j]);
-					stream << ' ';
-
-					table._fillStreamByChar(stream, ' ', ' ', table._rightIndent);
-				}
+				stream << std::right << table._chars.leftRightSimple << std::endl;
 			}
-			stream << std::right << table._chars.leftRightSimple << std::endl;
 
 			// Down border
 			if (i == table._rowSize - 1) {
 				stream << table._chars.downLeft;
-				table._fillStreamByChar(stream, table._chars.topDownSimple, columnWidth[0] + 1 + table._leftIndent + table._rightIndent);
+				ConsoleTable::_fillStreamByChar(stream, table._chars.topDownSimple, columnWidth[0] + 1 + table._leftIndent + table._rightIndent);
 
 				if (table._columnSize != 1) {
 					for (size_t j = 1; j < table._columnSize; j++) {
@@ -358,7 +452,7 @@ namespace samilton {
 			}
 			else {
 				stream << table._chars.leftSeparation;
-				table._fillStreamByChar(stream, table._chars.topDownSimple, columnWidth[0] + 1 + table._leftIndent + table._rightIndent);
+				ConsoleTable::_fillStreamByChar(stream, table._chars.topDownSimple, columnWidth[0] + 1 + table._leftIndent + table._rightIndent);
 
 				if (table._columnSize != 1) {
 					for (size_t j = 1; j < table._columnSize; j++) {
