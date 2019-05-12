694ac60a11706bd7f9edbc43938653834db84391
childcity <skulazkiy@gmail.com>
2019-05-12 03:23:07 +0300

    fix example for building under g++

--- Example.cpp
+++ Example.cpp
@@ -11,11 +11,21 @@ int main()
 
 	// Assign data to table
 	table[0][0] = "Some char string";
+	table[1][0] = '2'; // char
 	table[1][3] = true; // boolean data
 	table[2](samilton::Alignment::right)[1] = 10; // int data with own row alignment to right
 	table[3][3] = std::string("Some std::string");
 	table[2][2] = 2.354; // double data
 	table[0][1](samilton::Alignment::left) = "Some char multi string.\nWorks fine!\nEven with own alignment";
+
+	ConsoleTable::TableChars chars{'+', '+', '+', '+',
+								   '-', '+', '+',
+								   '|', '+', '+',
+								   '+'}; // creating TableChars struct with standard symbols setup
+	table.setTableChars(chars); // set TableChars struct as new symbol setup
+
+	// Output table
+	std::cout << table << std::endl;
 
 	table.clear(); // clear all table cells
 	table.addColumn({ 1, 2, 3 }); // add new column to table with elements
@@ -27,12 +37,13 @@ int main()
 	table.setAlignment(samilton::Alignment::centre); // set alignment of all table
 	table.setIndent(2, 1); // set indent of all table
 
-	ConsoleTable::TableChars chars; // creating TableChars struct with standard symbols setup
+	ConsoleTable::TableChars chars2; // creating TableChars struct with standard symbols setup
 	chars.centreSeparation = '+'; // changing symbol in struct
-	table.setTableChars(chars); // set TableChars struct as new symbol setup
+	table.setTableChars(chars2); // set TableChars struct as new symbol setup
+
 
 	// Output table
 	std::cout << table << std::endl;
 
-	system("pause");
+	//std::cin.get();
 }
